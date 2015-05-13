96c766b6dd551b29e0d0ec4d2212f88030a60a49
asc <ansicheng@163.com>
2015-05-13 19:58:15 +0800

    0.1.1.5-12 1. 修改crc16位与32位的生成函数与验证函数 2. 加入modbus的crc16算法

--- include/Crc.h
+++ include/Crc.h
@@ -1,10 +1,11 @@
 #ifndef CRC_H_INCLUDED
 #define CRC_H_INCLUDED
 
+int modbus_crc16(const unsigned char *puchMsg,int usDataLen, unsigned char *crc);
 
-bool make_crc16_code(const unsigned char *src, int len, unsigned char *crc);
+int make_crc16_code(const unsigned char *src, int len, unsigned char *crc);
 
-bool make_crc32_code(const unsigned char *src, int len, unsigned char *crc);
+int make_crc32_code(const unsigned char *src, int len, unsigned char *crc);
 
 bool verify_crc16_data(unsigned char *src, int len, const unsigned char *crc);
 
